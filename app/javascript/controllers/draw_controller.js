971e7acaf7029cb4c40cbdfcd5caba8c5bddc542
MeloThalisson <thalisson.melo@fretadao.unb>
2020-08-04 22:41:07 -0300

    Add canvas to dontdraw

--- app/javascript/controllers/draw_controller.js
+++ app/javascript/controllers/draw_controller.js
@@ -1,8 +1,29 @@
 import { Controller } from 'stimulus'; 
 import {application} from 'controllers';
+import {fabric} from 'fabric';
 
 application.register('draw', class extends Controller {
   connect() {
-    console.log("AAAAAAAAAAAAAAAA");
+    function run() {
+      let canvas = new fabric.Canvas('myCanvas', { isDrawingMode: true });
+      let rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+      });
+    }
+    
+    run();
+    
+  }
+
+  changeColor() {
+    this.canvas.freeDrawingBrush.color = this.colorValue;
+  }
+
+  canvas() {
+    return document.getElementById("mycanvas").fabric;
   }
 });
